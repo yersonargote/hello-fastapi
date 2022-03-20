b0b5be2ff95f422cd8d5526b9828f47efe31fb72
Yerson <llalex.2045@gmail.com>
2022-03-19 23:26:31 -0500

    Adding Body, Path, Query... better code, adding first method with Query

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-from typing import Optional
-from fastapi import FastAPI, status, Response
-from pydantic import BaseModel
-from typing import List, Dict
+from importlib.resources import path
+from typing import List, Optional
+from fastapi import Body, FastAPI, Path, Query, status, Response
+from pydantic import BaseModel, Field
+from typing import Dict
 
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
+    name: str = Field(..., min_length=2, max_length=50)
+    description: Optional[str] = Field(None, min_length=3, max_length=500)
+    price: float = Field(..., gt=0, le=100000000)
 
 
 class ItemOut(Item):
-    id: str
+    id: str = Field(..., min_length=2, max_length=50)
 
 
 items: Dict[str, Item] = {}
 
 
 @app.get(
-    "/items",
+    path="/items",
     status_code=status.HTTP_200_OK
 )
 def read_all_items():
@@ -29,10 +30,13 @@ def read_all_items():
 
 
 @app.get(
-    "/items/{item_id}",
+    path="/items/{item_id}",
     status_code=status.HTTP_200_OK
 )
-def read_item(item_id: str, response: Response = Response):
+def read_item(
+    item_id: str = Path(...),
+    response: Response = Response
+):
     try:
         return items[item_id]
     except KeyError:
@@ -41,10 +45,14 @@ def read_item(item_id: str, response: Response = Response):
 
 
 @app.post(
-    "/items/{item_id}",
+    path="/items/{item_id}",
     status_code=status.HTTP_201_CREATED
 )
-def create_item(item_id: str, item: Item, response: Response = Response):
+def create_item(
+    item_id: str = Path(...),
+    item: Item = Body(...),
+    response: Response = Response
+):
     if item_id in items.keys():
         response.status_code = status.HTTP_409_CONFLICT
         return {
@@ -57,10 +65,14 @@ def create_item(item_id: str, item: Item, response: Response = Response):
 
 
 @app.put(
-    "/items/{item_id}",
-    status_code=status.HTTP_200_OK
+    path="/items/{item_id}",
+    status_code=status.HTTP_200_OK,
 )
-def update_item(item_id: str, item: Item, response: Response = Response):
+def update_item(
+    item_id: str = Path(...),
+    item: Item = Body(...),
+    response: Response = Response
+):
     if item_id in items.keys():
         items[item_id] = item
         return items[item_id]
@@ -71,3 +83,23 @@ def update_item(item_id: str, item: Item, response: Response = Response):
             "item_id": item_id,
             "item": item
         }
+
+
+@app.get(
+    path="/items/",
+    status_code=status.HTTP_200_OK
+)
+def get_item_by_price(
+    price: float = Query(...),
+    limit: int = Query(..., gt=0, le=100),
+    response: Response = Response
+):
+    if price < 0 or limit < 0:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "detail": "Invalid query parameters",
+            "price": price,
+            "limit": limit
+        }
+    filter: List = [item for item in items.values() if item.price == price]
+    return filter[:limit]
