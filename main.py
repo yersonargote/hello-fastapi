f5f69a0014dc4165205030605d17987b96eccc42
Yerson <llalex.2045@gmail.com>
2022-03-21 20:04:01 -0500

    Change: return dict to list in items. Adding: response model return. Change: return error to raise error.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import Body, FastAPI, Path, Query, status, Response
+from fastapi import Body, FastAPI, Path, Query, status, HTTPException
 from pydantic import BaseModel, Field
 from typing import Dict
 
@@ -9,8 +9,10 @@ app = FastAPI()
 
 class Item(BaseModel):
     id: str = Field(..., title="The item's unique ID")
-    name: str = Field(..., min_length=2, max_length=50, title="The item's name")
-    description: Optional[str] = Field(None, min_length=3, max_length=500, title="The item's description")
+    name: str = Field(..., min_length=2, max_length=50,
+                      title="The item's name")
+    description: Optional[str] = Field(
+        None, min_length=3, max_length=500, title="The item's description")
     price: float = Field(..., gt=0, le=100000000, title="The item's price")
     stock: int = Field(..., gt=0, le=100, title="The item's stock")
 
@@ -20,41 +22,40 @@ items: Dict[str, Item] = {}
 
 @app.get(
     path="/items",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    response_model=List[Item],
 )
 def read_all_items():
-    return items
+    return list(items.values())
 
 
 @app.get(
     path="/items/{item_id}",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    response_model=Item,
 )
 def read_item(
-    item_id: str = Path(...),
-    response: Response = Response
+    item_id: str = Path(...)
 ):
-    try:
-        return items[item_id]
-    except KeyError:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": "Item not found"}
+    if item_id not in items:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Item not found")
+    return items[item_id]
 
 
 @app.post(
     path="/items/",
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
+    response_model=Item,
 )
 def create_item(
-    item: Item = Body(...),
-    response: Response = Response
+    item: Item = Body(...)
 ):
     if item.id in items.keys():
-        response.status_code = status.HTTP_409_CONFLICT
-        return {
-            "detail": "Item already exists",
-            "item": item
-        }
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Item already exists",
+        )
     items[item.id] = item
     return items[item.id]
 
@@ -62,39 +63,34 @@ def create_item(
 @app.put(
     path="/items/{item_id}",
     status_code=status.HTTP_200_OK,
+    response_model=Item,
 )
 def update_item(
     item_id: str = Path(...),
     item: Item = Body(...),
-    response: Response = Response
 ):
-    if item_id in items.keys():
-        items[item_id] = item
-        return items[item_id]
-    else:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {
-            "detail": "Item not found",
-            "item_id": item_id,
-            "item": item
-        }
+    if item_id not in items.keys():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Item not found",
+        )
+    items[item_id] = item
+    return items[item_id]
 
 
 @app.get(
     path="/items/",
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    response_model=List[Item],
 )
 def get_item_by_price(
     price: float = Query(...),
     limit: int = Query(..., gt=0, le=100),
-    response: Response = Response
 ):
     if price < 0 or limit < 0:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {
-            "detail": "Invalid query parameters",
-            "price": price,
-            "limit": limit
-        }
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid price or limit",
+        )
     filter: List = [item for item in items.values() if item.price == price]
     return filter[:limit]
